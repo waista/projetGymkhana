83901659c4a10468189758d1f3522f039d7df6be
waista <gi.peneau@hotmail.co.uk>
2016-10-30 02:39:25 +0200

    Une nouvelle mise a jour
    On commence a avoir un truc qui ressemble a un jeu. Mais bon pour l'instant pas grand chose de testé ou validé

--- Gymkhana.java
+++ Gymkhana.java
@@ -113,7 +113,82 @@ Class Gymkhana{
 	       scanIn.close();            
 	       System.out.println(sWhatever);
 	}
+	public int[] recupDonnee(){
+		//fonction qui renvoie un tableau a trois cases : x et y les coordonnées du pion a placer et z le sens du pion
+		//je ne sais pas encore récuperer les données de la ligne de commande, il me faut une fonction type scanf, a voir plus tard
 
+		//ne pas oublier de vérifier les valeurs entrees (bien trois entiers) et z bien o ou 1
+
+	}
+
+
+	/*
+	---------------------------------------------------------------------------------------------------------------------------
+													Fonctions générales du systeme de jeu
+	---------------------------------------------------------------------------------------------------------------------------
+	*/
+
+	public int entierASauvgarder(int joueur, int sens){
+		//fonction qui renvoie l'entier a enregistrer dans le plateau dépendant du sens du pion et du joueur qui le place
+		//le joueur doit etre 1 ou 2 et le sens doit etre o ou 1
+		int x = 0;
+		if (joueur ==2) {
+			x +=2
+		}
+		if (sens == 1) {
+			x++;
+		}
+		return x;
+	}
+
+	public int[] unTour(int joueur){
+		//fonction qui geres le tour d'un joueur dans la partie
+		//renvoie les donnees du pion joué par le joueur pendant le tour
+
+		boolean verif = false;
+		int[] donnes = new int[3];
+		while (!verif) {
+			donnes = recupDonnee();
+			verif = gestionDesVerifs();
+		}
+		int x = entierASauvgarder(joueur, donnes[2]);
+		this.plateau[donnes[0]][donnes[1]] = x;
+	}
+
+	public void jeu(){
+		//fonction qui geres le déroulement du jeu en son entiereté
+		System.out.println("Bonjour et bienvenu au magnifique jeu du Gymkhana !");
+		System.out.println("Petite remarque : cette version du jeu est encore en alpha,n'attendez pas grand chose de nous");
+		int finDuJeu = 0;//0 : partie en cours; 1:joueur 1 a gagné; 2:joueur 2 a gagné; 3 : égalité
+		int cpt = 0;
+		while (finDuJeu = 0) {
+			unTour(cpt%2);
+			cpt++;
+			if (aGagne(donnes)) {
+				finDuJeu = cpt%2;
+			}
+			if (plateauRempli()) {
+				finDuJeu = 3;
+			}
+		}
+
+
+	}
+
+
+	/*
+	---------------------------------------------------------------------------------------------------------------------------
+													Fonctions a réaliser
+	---------------------------------------------------------------------------------------------------------------------------
+	*/
+
+	public boolean aGagne(int[] donnes){
+		//fonction qui renvoie vrai si le dernier pion placé réalise une ligne d'un coté a l'autre du plateau et faux sinon
+	}
+
+	public boolean plateauRempli(){
+		//fonction qui renvoie vrai si le plateau ne permet pas de jouer de pion supplémentaire et faux sinon
+	}
 
 	/*
 	---------------------------------------------------------------------------------------------------------------------------
