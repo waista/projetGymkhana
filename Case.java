c7b411b0a3f30819be178b844b7b8ad8cc11c018
waista <gi.peneau@hotmail.co.uk>
2016-11-29 10:37:30 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -5,15 +5,18 @@ abstract class Case{
 
 	private int coordx;
 	private int coordy;
+	protected int joueur;
 
 	protected Case(){
 		int coordx;
 		int coordy;
+		int joueur;
 	}
 
-	protected Case(int x,int y){
+	protected Case(int x,int y, int j){
 		coordx = x;
 		coordy = y;
+		joueur = j;
 	}
 
 	public int getCoordx(){
@@ -24,6 +27,19 @@ abstract class Case{
 		return coordy;
 	}
 
+	public int getJoueur(){
+		return joueur;
+	}
+
+	public void afficherJoueur(){
+		System.out.print(joueur);
+	}
+
+	// public ClassesConnexite getClasseConnexite(){
+	// 	return maClasse;
+	// }
+
 	abstract public String toString();
+	abstract public boolean jeSuisUnNoeud();
 
 }
