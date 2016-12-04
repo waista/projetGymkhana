25b1c3905d5d15e79744e668fdbfa4ceb12dc4fd
waista <gi.peneau@hotmail.co.uk>
2016-12-04 20:11:33 +0100

    Update Case.java

--- Case.java
+++ Case.java
@@ -1,11 +1,13 @@
 import java.util.*;
 
 
-abstract class Case{
-
-	private int coordx;
-	private int coordy;
+class Case{
+	// a supprimer 
+	private int coordx;//private
+	private int coordy;//private
+	private ClassesConnexite maClasse;//private
 	protected int joueur;
+
 
 	protected Case(){
 		int coordx;
@@ -17,6 +19,13 @@ abstract class Case{
 		coordx = x;
 		coordy = y;
 		joueur = j;
+	}
+
+	protected Case(int x,int y, int j, ClassesConnexite cl){
+		coordx = x;
+		coordy = y;
+		joueur = j;
+		maClasse =	cl;
 	}
 
 	public int getCoordx(){
@@ -31,15 +40,39 @@ abstract class Case{
 		return joueur;
 	}
 
+	public ClassesConnexite getClasseConnexite(){
+		return maClasse;
+	}
+
+	//fonction de test
+	public int getIDClasseConnexite(){
+		if (maClasse != null) {
+			return maClasse.getID();
+		}else{
+			return -1;
+		}
+	}
+
 	public void afficherJoueur(){
 		System.out.print(joueur);
 	}
 
-	// public ClassesConnexite getClasseConnexite(){
-	// 	return maClasse;
-	// }
+	public void setClasse(ClassesConnexite nouvelleClasse){
+		maClasse = nouvelleClasse;
+	}
 
-	abstract public String toString();
-	abstract public boolean jeSuisUnNoeud();
+
+	public String toString(){
+		if (joueur == 1) {
+			return "R";
+		}else{
+			return "b";
+		}
+	}
+
+
+	public boolean jeSuisUnNoeud(){
+		return false;
+	}
 
 }
